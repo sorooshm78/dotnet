2105b2dc7b5371b0bec46b46b81e847458c0869f
Soroush mohammadi <sorooshm78@gmail.com>
2024-03-12 18:59:31 +0330

    del

--- Projects/SimpleBook/SimpleBookWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SimpleBookWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
